eb0ef2c3cb1284cfa56317061e9bd56ecc461a93
muhammad-mohsin-siddique <muhammadmohsinsiddique789@gmail.com>
2023-09-17 19:46:20 +0500

    now code implementation with method

--- FahrenheitToCelsius.java
+++ FahrenheitToCelsius.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class FahrenheitToCelsius {
 
+    public double fahrenToCelsius(double fahrenheitTemperature) // method that calculate the value and return the result 
+    {
+    
+        return ((5.0/9.0) * (fahrenheitTemperature - 32.0));
+    
+    }
+
     public static void main(String[] args) {
   
         Scanner userInput = new Scanner(System.in);
@@ -9,12 +16,12 @@ public class FahrenheitToCelsius {
         System.out.print("\nEnter Fahrenheit Temperature: ");
         
         double fahrenheit = userInput.nextDouble();
+
+        FahrenheitToCelsius Main_class_object = new FahrenheitToCelsius(); // creat an object of main class to access  methods
     
-        double celsius = ((5.0/9.0) * (fahrenheit - 32.0));
+        double celsius = Main_class_object.fahrenToCelsius(fahrenheit);
     
         System.out.println("Fahrenheit Temperature = " + fahrenheit + " and " + "Convert into Celsius = " + celsius);
-    
-        System.out.println("\n" + 5/9);
 
         userInput.close();
     }
